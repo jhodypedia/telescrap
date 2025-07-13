c5118b1de0e176a200cb1c033731cfac525903a0
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-07-13 22:15:52 +0700

    Create login.js

---
+++ views/login.js
@@ -0,0 +1,9 @@
+<div class="container"><div class="col-md-6 mx-auto">
+<h2>Login</h2>
+<form method="POST" class="card p-4">
+  <input name="username" class="form-control" placeholder="Username" required>
+  <input name="password" type="password" class="form-control mt-2" placeholder="Password" required>
+  <button class="btn btn-primary w-100 mt-3">Login</button>
+</form>
+<p class="text-center mt-2">Don't have account? <a href="/register">Register</a></p>
+</div></div>
